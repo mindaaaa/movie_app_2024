3ec2cc8ce67f4aabcbc9178e7d4574bb2b996c81
mindaaaa <devalc735@gmail.com>
2024-05-25 20:40:37 +0900

    movie_app_2024 router 적용하기

---
+++ src/routes/About.js
@@ -0,0 +1,12 @@
+import './About.css';
+
+function About() {
+  return (
+    <div className='about__container'>
+      <span>"When you have faults, do not fear to abandon them."</span>
+      <span>- Confucius, B.C 551-479</span>
+    </div>
+  );
+}
+
+export default About;
